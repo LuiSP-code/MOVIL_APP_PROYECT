ddb45597b7dfe73549fd0945d0b06787157ed655
Luis PS <luis03sanches@outlook.com>
2026-05-16 15:41:18 -0600

    Historial de Gastos

--- lib/pantallas/historial_pantalla.dart
+++ lib/pantallas/historial_pantalla.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:finantial_app/configuracion/supabase_config.dart';
 
-class HistorialPantalla extends StatelessWidget {
-  const HistorialPantalla({super.key});
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Historial de Gastos\n(Asignada a Luis)", textAlign: TextAlign.center),
-      ),
-    );
-  }
+  State<ListScreen> createState() => _ListScreenState();
 }
+// VENTANA: HISTORIAL DE GASTOS
+class _ListScreenState extends State<ListScreen> {
+  final supabase = Supabase.instance.client;
+
+  // ICONOS PARA LAS CATEGORÍAS
+  IconData _getIcon(String categoria) {
+    switch (categoria) {
+      case 'Alimentos':
+        return Icons.restaurant;
+      case 'Transporte':
+        return Icons.directions_bus;
+      case 'Ocio':
+        return Icons.movie;
+      case 'Salud':
+        return Icons.medical_services;
+      case 'Educación':
+        return Icons.school;
+      case 'Subscripciones':
+        return Icons.subscriptions;
+      default:
+        return Icons.attach_money;
+    }
+  }
